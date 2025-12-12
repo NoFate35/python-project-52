fccf5a37299b8b20e8603584cb3d99920bfdee25
NoFate35 <vsokol35@mail.ru>
2025-12-12 23:23:24 +0300

    Changes to be committed: 	modified:   task_manager/settings.py 	modified:   task_manager/templates/index.html
    step 1 complete

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -125,5 +125,4 @@ if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
     if RENDER_EXTERNAL_HOSTNAME:
-        ALLOWED_HOSTS = []
-        ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+        ALLOWED_HOSTS = [RENDER_EXTERNAL_HOSTNAME]
