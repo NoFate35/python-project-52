50f75b6551b77564be08113a9d3af3554a1f52c2
NoFate35 <vsokol35@mail.ru>
2025-12-12 20:52:14 +0300

    400 problem

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['webserver', '127.0.0.1',  '*']
 
 
 # Application definition
