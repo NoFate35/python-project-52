1bc4026dbc7514b174f10e098efd95fb8e14d24d
NoFate35 <vsokol35@mail.ru>
2025-12-12 19:37:18 +0300

    ico problem

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from task_manager import views
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     path("", views.index),
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico')))
 ]
