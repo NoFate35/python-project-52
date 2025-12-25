703fd8e55878dcaf937fcbb1127b3a864a67eb56
NoFate35 <vsokol35@mail.ru>
2025-12-25 18:02:09 +0300

    Changes to be committed: 	modified:   task_manager/templates/index.html 	modified:   task_manager/templates/login_form.html 	modified:   task_manager/views.py
    work for add flash messages

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,8 +1,8 @@
 from django.views.generic.base import TemplateView
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
-from django.shortcuts import redirect, render
-from django.urls import reverse_lazy
+from django.urls import reverse
+from django.contrib import messages
 
 
 class IndexView(TemplateView):
@@ -18,18 +18,5 @@ class LoginUserView(LoginView):
     template_name = 'login_form.html'
     
     def get_success_url(self):
-        return reverse_lazy('home')
-    
-    #def form_invalid(self, form):
-        #return super().form_invalid(form)
-
-    '''
-    def post(self, request, *args, **kwargs):
-        form = AuthenticationForm(request.POST)
-        print('ffform', form.cleaned_data)
-        if form.is_valid():
-            print('ffform', form.cleaned_data)
-            form.save()
-            return redirect('home')    
-        return render (request, 'login_form.html', {'form': form})
-    '''
+        messages.add_message(self.request, messages.SUCCESS, 'Вы залогинены')
+        return reverse('home')
