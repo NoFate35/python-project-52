a09c61be8a6dc65757080dcfa686b3de56ae9f8f
NoFate35 <vsokol35@mail.ru>
2026-02-01 18:41:17 +0300

    Changes to be committed: 	modified:   Makefile 	new file:   task_manager/statuses/fixtures/statuses.json 	new file:   task_manager/statuses/migrations/0001_initial.py 	modified:   task_manager/statuses/models.py 	modified:   task_manager/statuses/tests.py 	modified:   task_manager/users/fixtures/users.json
    work for statuses tests

--- task_manager/statuses/tests.py
+++ task_manager/statuses/tests.py
@@ -1,3 +1,147 @@
 from django.test import TestCase
+from django.urls import reverse
+from task_manager.statuses.models import Status
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class UsersTest(TestCase):
+    fixtures = ["statuses.json", "users.json"]
+
+    def test_fixtures(self):
+        status = Status.objects.get(pk=1)
+        self.assertEqual(status.name, "status1")
+        statuses = Status.objects.all()
+        self.assertEqual(len(statuses), 3)
+
+        user = User.objects.get(pk=1)
+        self.assertEqual(user.username, "rrr")
+        users = User.objects.all()
+        self.assertEqual(len(users), 3)
+
+
+    def test_statuses_list(self):
+        response = self.client.get(reverse("statuses_list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("statuses", response.context)
+        statuses = response.context["statuses"]
+        self.assertTrue(len(statuses) == 3)
+
+
+    def test_status_create(self):
+        create_url = reverse("statuses_create")
+        response = self.client.get(create_url, follow=True,)
+        self.assertContains(response, 'Вы не авторизованы! Пожалуйста, выполните вход.')
+
+        response = self.client.post(create_url, follow=True, data={"name": "status4"})
+        self.assertContains(response, 'Вы не авторизованы! Пожалуйста, выполните вход.')
+        
+        user = User.objects.filter(pk="2")[0]
+        login_url = reverse("login")
+        response = self.client.post(login_url, follow=True, data={'username': user.username, 'password': '1234'})
+        self.assertContains(response, 'Вы залогинены')
+
+        create_url = reverse("statuses_create")
+        response = self.client.get(create_url)
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post(create_url, follow=True, data={"name": "status4"})
+        self.assertContains(response, 'Статус успешно создан')
+        list_url = reverse("statuses_list")
+        response = self.client.get(list_url)
+        self.assertContains(response, "status4")
+        statuses = response.context["statuses"]
+        self.assertTrue(len(statuses) == 4)
+
+
+
+
+
+
+    def test_user_bad_create(self):
+        create_url = reverse("users_create")
+        response = self.client.post(create_url, data={"first_name": "Bobik", "last_name": 'S', 'username': 'bob-S', 'password1':'123', 'password2':'1234'})
+        self.assertContains(response, 'Введенные пароли не совпадают.')
+
+
+
+
+
+
+    def test_bad_permission_middleware(self):
+        user = User.objects.filter(first_name="Егор")[0]
+        update_url = reverse("users_update", kwargs={'pk': user.id})
+        response = self.client.post(update_url, follow=True, data={"first_name": "Bobik", "last_name": 'Sokkkkk', 'username': 'bob-S', 'password1':'123', 'password2':'123'})
+        self.assertContains(response, 'Вы не авторизованы! Пожалуйста, выполните вход.')
+
+        login_url = reverse("login")
+        response = self.client.post(login_url, follow=True, data={'username': 'rrr', 'password':'1234'})
+        self.assertContains(response, 'Вы залогинены')
+
+        user = User.objects.filter(first_name="пупсик")
+        delete_url = reverse("users_delete", kwargs={'pk': user[0].id})
+        response = self.client.post(delete_url, follow=True)
+        self.assertContains(response, 'У вас нет прав для изменения другого пользователя.')
+
+
+
+
+
+
+
+    def test_user_update(self):
+        user = User.objects.filter(pk="2")[0]
+
+        login_url = reverse("login")
+        response = self.client.post(login_url, follow=True, data={'username': user.username, 'password': '1234'})
+        self.assertContains(response, 'Вы залогинены')
+
+        update_url = reverse("users_update", kwargs={'pk': user.id})
+        response = self.client.get(update_url)
+
+        self.assertEqual(response.status_code, 200)
+        response = self.client.post(update_url, follow=True, data={"first_name": user.first_name, "last_name": user.last_name, 'username': 'egoska', 'password1':'123', 'password2':'123'})
+        self.assertContains(response, 'Пользователь успешно изменен')
+        self.assertContains(response, 'egoska')
+
+
+
+
+
+
+
+    def test_user_bad_update(self):
+        user = User.objects.filter(pk="2")[0]
+
+        login_url = reverse("login")
+        response = self.client.post(login_url, follow=True, data={'username': user.username, 'password': '1234'})
+        self.assertContains(response, 'Вы залогинены')
+
+        update_url = reverse("users_update", kwargs={'pk': user.id})
+        response = self.client.post(update_url, follow=True, data={"first_name": user.first_name, "last_name": user.last_name, 'username': 'egoska', 'password1':'12', 'password2':'12'})
+        self.assertContains(response, 'Введённый пароль слишком короткий. Он должен состоять из как минимум 3 символа.')
+
+
+
+
+
+
+
+    def test_user_delete(self):
+        user = User.objects.filter(pk="2")[0]
+
+        login_url = reverse("login")
+        response = self.client.post(login_url, follow=True, data={'username': user.username, 'password': '1234'})
+        self.assertContains(response, 'Вы залогинены')
+
+        delete_url = reverse("users_delete", kwargs={'pk': user.id})
+        response = self.client.get(delete_url)
+        self.assertEqual(response.status_code, 200)
+        response = self.client.post(delete_url, follow=True)
+        self.assertContains(response, 'Пользователь успешно удален')
+
+        list_url = reverse("users_list")
+        response = self.client.get(list_url)
+        users = response.context["users"]
+        self.assertTrue(len(users) == 2)
+
+    
