0ad2bad25ea799b9f21ff1c626d178e730e6e901
NoFate35 <vsokol35@mail.ru>
2026-03-06 22:06:38 +0300

    Changes to be committed: 	modified:   task_manager/tasks/fixtures/tasks.json 	new file:   task_manager/tasks/forms.py 	modified:   task_manager/tasks/models.py 	new file:   task_manager/tasks/templates/tasks/create_form.html 	new file:   task_manager/tasks/templates/tasks/delete.html 	new file:   task_manager/tasks/templates/tasks/task_list.html 	modified:   task_manager/tasks/tests.py
    work for tasks app

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Task(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
     description = models.TextField(blank=True)
     status = models.ForeignKey(Status, on_delete=models.PROTECT)
     author = models.ForeignKey(User, on_delete=models.PROTECT, related_name="authors")
-    executor = models.ForeignKey(User, on_delete=models.PROTECT, related_name="executors")
+    executor = models.ForeignKey(User, on_delete=models.PROTECT, related_name="executors", blanc=True)
     #label = models.ManyToManyField(Label, on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
--- task_manager/tasks/tests.py
+++ task_manager/tasks/tests.py
@@ -1,12 +1,14 @@
 from django.test import TestCase
 from task_manager.tasks.models import Task
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class TasksTest(TestCase):
     fixtures = ["tasks.json", "users.json", "statuses.json"]
 
     def setUp(self):
+        self.list_url = self.list_url = reverse("tasks_list")
         self.task1 = Task.objects.get(name='task1')
         self.tasks = Task.objects.all()
     
@@ -14,4 +16,16 @@ class TasksTest(TestCase):
         #проверка статуса по названию, запрос из базы
         self.assertEqual(self.task1.name, "task1")
         #по общему количеству, запрос из базы
-        self.assertEqual(len(self.tasks), 1)
+        self.assertEqual(len(self.tasks), 2)
+    
+    def test_tasks_list(self):
+        #отображение списка пользователей
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Задачи')
+
+        #по общему количеству
+        self.assertIn("tasks", response.context)
+        tasks = response.context["tasks"]
+        self.assertTrue(len(tasks) == 2)
+    
