b173475d89605a2061cb15e5e57f8d58e2320c6c
NoFate35 <vsokol35@mail.ru>
2026-03-07 13:57:57 +0300

    Changes to be committed: 	modified:   task_manager/tasks/forms.py
    work for task app

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -2,12 +2,15 @@ from django import forms
 from .models import Task
 from task_manager.statuses.models import Status
 from django.contrib.auth.models import User
+#from task_manager.labels.models import Label
 
 
 class ProductForm(forms.ModelForm):
     description = forms.CharField(widget=forms.Textarea, required=False)
     status = forms.ModelChoiceField(queryset=Status.objects.all(), label="Статус")
     executor = forms.ModelChoiceField(queryset=User.objects.all(), label="Исполнитель")
+    #labels = forms.MultipleChoiceField(queryset=Label.objects.all(), label="Метки")
+    
 
     class Meta:
         model = Task
