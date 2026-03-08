cd2126f370a8c84a708c0859cf3f0a3372eae52f
NoFate35 <vsokol35@mail.ru>
2026-03-08 21:56:17 +0300

    Changes to be committed: 	modified:   task_manager/statuses/forms.py 	modified:   task_manager/statuses/templates/statuses/create_form.html 	modified:   task_manager/statuses/views.py
    work for statuses create html

--- task_manager/statuses/forms.py
+++ task_manager/statuses/forms.py
@@ -6,6 +6,7 @@ from django import forms
 class RegisterStatusForm(forms.ModelForm):
 
     name = forms.CharField(label="Имя", label_suffix="")
+    auto_id=True
 
 
     class Meta:
@@ -19,21 +20,33 @@ class RegisterStatusForm(forms.ModelForm):
                                                  'maxlength':"100",
                                                  'class':"form-control", 
                                                  'placeholder':"Имя",
-                                                 'required':"",
-                                                 'id':"id_name"
+                                                 'required':""
                                                  })
-   
-
+        if self.errors:
+            self.fields['name'].widget.attrs.update({'class':"form-control is-invalid"})
 
 
 
 
 class UpdateStatusForm(forms.ModelForm):
 
+    name = forms.CharField(label="Имя", label_suffix="")
+
     class Meta:
         model = Status
         fields = ['name']
     
-    def clean_username(self):
+    def clean_name(self):
         name = self.cleaned_data['name']
         return name
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'type':"text",
+                                                 'name':"name",
+                                                 'maxlength':"100",
+                                                 'class':"form-control", 
+                                                 'placeholder':"Имя",
+                                                 'required':"",
+                                                 'id':"id_name"
+                                                 })
