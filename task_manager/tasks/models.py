b5f1b7354af84ee963a5592615a65cccfa027dc8
NoFate35 <vsokol35@mail.ru>
2026-03-20 12:49:35 +0300

    Changes to be committed: 	modified:   task_manager/labels/migrations/0001_initial.py 	modified:   task_manager/tasks/migrations/0001_initial.py 	deleted:    task_manager/tasks/migrations/0002_remove_task_created_at.py 	deleted:    task_manager/tasks/migrations/0003_task_created_at.py 	deleted:    task_manager/tasks/migrations/0004_alter_task_executor_alter_task_name.py 	deleted:    task_manager/tasks/migrations/0005_task_label.py 	deleted:    task_manager/tasks/migrations/0006_rename_label_task_labels.py 	deleted:    task_manager/tasks/migrations/0007_alter_task_labels.py 	modified:   task_manager/tasks/models.py
    add "task_labels_relation"

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -4,11 +4,20 @@ from task_manager.labels.models import Label
 from django.contrib.auth.models import User
 
 # Create your models here.
+
 class Task(models.Model):
     name = models.CharField(max_length=200, unique=True)
     description = models.TextField(blank=True)
     status = models.ForeignKey(Status, on_delete=models.PROTECT)
     author = models.ForeignKey(User, on_delete=models.PROTECT, related_name="authors")
     executor = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True, related_name="executors")
-    labels = models.ManyToManyField(Label, blank=True, related_name='labels')
+    labels = models.ManyToManyField(Label)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+
+"""
+
+class LabelTask(models.Model):
+    label = models.ForeignKey(Label, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+"""
