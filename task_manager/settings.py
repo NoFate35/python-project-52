6fcff985d1f8744433d784bcfc0090c4e7df415d
NoFate35 <vsokol35@mail.ru>
2026-04-21 15:50:01 +0300

    Changes to be committed: 	modified:   task_manager/settings.py
    work for github actions allowed hosts

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -26,9 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", False)
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
-if ALLOWED_HOSTS is None:
-    ALLOWED_HOSTS = 'localhost'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+
 
 # Application definition
 
